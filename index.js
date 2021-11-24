ea9e704bbbd435e2d167038b8cd93089c690f911
QTPah <62460312+QTPah@users.noreply.github.com>
2021-11-24 21:10:39 +0100

    x

--- index.js
+++ index.js
@@ -8,48 +8,35 @@ s.write('Starting');
 let digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 const fetch = require('node-fetch');
 
-function isValid(code) {
-  return fetch('https://kahoot.it/reserve/session/'+code);
-}
+let maxThreads = 2;
+let threads = 0;
 
 function spawnBot(code) {
   const client = new Kahoot();
   client.join(code, "PinLeaker").catch(err=>{
-    s.clearLine();
-    s.cursorTo(0);
-      process.stdout.write(code + ` (\u001b[31;1mINVALID\u001b[0m) [${tm.min}:${tm.ms}]\n`);
-      tm.reset();
-      x();
+    console.log(code);
   });
   client.on("Joined", () => {
-    s.clearLine();
-    s.cursorTo(0);
-    process.stdout.write(code + ` (\u001b[32;1mVALID\u001b[0m) [${tm.min}:${tm.ms}]\n`);
+    console.log(code);
     client.leave();
-    tm.reset();
-    x();
   });
 }
 
-function x() {
-  let code = '';
-  for(i=0; i<6; i++) {
-    code += digits[Math.floor(Math.random()*10)];
-  }
-  fetch('https://kahoot.it/reserve/session/'+code).then(res => {
-    if(res.status == 200) {
-      tm.startTimer();
-      s.clearLine();
-      s.cursorTo(0);
-      s.write(code + ' (\u001b[33;1mPENDING...\u001b[0m)');
-      spawnBot(code);
-    } else {
-      s.clearLine();
-      s.cursorTo(0);
-      s.write(code+'');
-      x();
+while(true){
+  if(threads < maxThreads) {
+    let code = '';
+    for(i=0; i<6; i++) {
+      code += digits[Math.floor(Math.random()*10)];
     }
-  });
+    threads += 1;
+    fetch('https://kahoot.it/reserve/session/'+code).then(res => {
+      threads -= 1;
+      console.log(threads);
+      if(res.status == 200) {
+        spawnBot(code);
+      } else {
+        console.log(threads);
+      }
+    });
+  }
 }
-
-x();
